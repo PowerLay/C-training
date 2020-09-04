6b860fc00357e5f6a102c206033f1eb362955d6c
PowerLay <1854pro@gmail.com>
2020-09-04 16:59:33 +0300

    Start

---
+++ TimerEvents/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace TimerEvents
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Timer timer { get; set; }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            timer = new Timer { Interval = 1000 };
+            timer.Tick += Timer_tick;
+            timer.Start();
+        }
+
+        private void Timer_tick(object sender, EventArgs e)
+        {
+            progressBar1.Value += 10;
+            if (progressBar1.Value>=100)
+            {
+                timer.Stop();
+            }
+        }
+    }
+}
